26eea23a1a3bc4d54d82f4102fa291427727971d
DiegoBarajas <desaubv@gmail.com>
2024-06-26 16:55:01 -0600

    Added phone list items on navbar links

--- src/Fragments/Navbar.js
+++ src/Fragments/Navbar.js
@@ -3,10 +3,16 @@ import '../Styles/Fragments/Navbar.css'
 import { Link } from 'react-router-dom'
 import Logo from '../Components/Logo';
 
+import ItemNavbar from '../Components/ItemNavbar';
+
+import SearchNavbar from '../Components/SearchNavbar';
 import sellSvg from '../Assets/Icons/sell.svg'
 import loginSvg from '../Assets/Icons/login.svg'
-import ItemNavbar from '../Components/ItemNavbar';
-import SearchNavbar from '../Components/SearchNavbar';
+import categorySvg from '../Assets/Icons/category.svg'
+import createAccountSvg from '../Assets/Icons/create_account.svg'
+import reportSvg from '../Assets/Icons/report.svg'
+import helpSvg from '../Assets/Icons/help.svg'
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -25,17 +31,63 @@ const Navbar = () => {
 
             <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
 
-                <ItemNavbar 
-                    to='/'
-                    icon={sellSvg} 
-                    direction={isMenuOpen ? 'vertical' : 'horizontal'}
-                >Quiero vender</ItemNavbar>
+                
 
-                <ItemNavbar 
-                    to='/' 
-                    icon={loginSvg} 
-                    direction={isMenuOpen ? 'vertical' : 'horizontal'}
-                >Iniciar sesión</ItemNavbar>
+                {
+                    isMenuOpen
+                    ? <>
+                        <ItemNavbar 
+                            to='/'
+                            icon={sellSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Quiero vender</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/'
+                            icon={categorySvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Categorias</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/' 
+                            icon={loginSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Iniciar sesión</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/' 
+                            icon={createAccountSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Crear cuenta</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/' 
+                            icon={reportSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Reportar un problema</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/' 
+                            icon={helpSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Preguntas frecuentes</ItemNavbar>
+                    </>
+                    : <>
+                        <ItemNavbar 
+                            to='/'
+                            icon={sellSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Quiero vender</ItemNavbar>
+
+                        <ItemNavbar 
+                            to='/' 
+                            icon={loginSvg} 
+                            direction={isMenuOpen ? 'vertical' : 'horizontal'}
+                        >Iniciar sesión</ItemNavbar>
+                    </>
+                }
+
+                
             </ul>
             
             <div className="navbar-toggle" onClick={toggleMenu} title={isMenuOpen ? "Cerrar menú" : "Abir menú"}>
