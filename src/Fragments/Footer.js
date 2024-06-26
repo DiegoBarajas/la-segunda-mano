9db9ea3fd5b238d5ddf4f0f9b291151659acb889
DiegoBarajas <desaubv@gmail.com>
2024-06-26 17:01:35 -0600

    Added version on footer (2)

--- src/Fragments/Footer.js
+++ src/Fragments/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../Styles/Fragments/Footer.css'
 import { Link } from 'react-router-dom'
 
-import { version } from '../../package.json'
+import packageJSON from '../../package.json'
 
 const Footer = () => {
     return (
@@ -12,10 +12,10 @@ const Footer = () => {
                 <Link className='link-footer' to='/' title='Condiciones de uso'>Condiciones de uso</Link>
                 <Link className='link-footer' to='/' title='Politicas de privacidad'>Politicas de privacidad</Link>
                 <Link className='link-footer' to='/' title='Preguntas frecuentes'>Preguntas frecuentes</Link>
-
             </section>
+
             <section className='info-footer'>
-                <p className='p-info-footer'>La segunda mano - 2024 | Version: {version}</p>
+                <p className='p-info-footer'>La segunda mano ( 2024 ) • Version: {packageJSON.version}</p>
             </section>
         </footer>
     )
