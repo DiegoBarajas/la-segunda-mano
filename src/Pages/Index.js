e44e6c96ca011b94c44507a5388681352b27fa4d
DiegoBarajas <desaubv@gmail.com>
2024-06-26 22:57:27 -0600

    Updated constants

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -11,6 +11,10 @@ const Index = () => {
         <StandardLayout>
             <ContententLayout>
                 <Title center>Hola Mundo</Title>
+                <button onClick={() => {
+                    const jeison = ["Tablet", "TV", "Celular", "La fusldas djkahs  oidjasjoidaslkdn nassaj hdoiasjdl kasjoidjasoi djasiodjasoidjdasoij dasoij doiasjdoiasj doiasjd oiasj doiasdjknd jkasndjkasndjkas ndjkasndjkansdjknasdjk djkahsj oidasjkhd jasdjkaskjdas jkda", "el pepe", "ete setch", "Julian"]
+                    localStorage.setItem('history', JSON.stringify(jeison))
+                }}>asmldkasd</button>
             </ContententLayout>
         </StandardLayout>
     )
